82f521e5aeee5d2f20c04857a64d02eb18e5b694
Balakimbiyou <126910392+Balakimbiyou@users.noreply.github.com>
2025-11-19 01:24:34 +0100

    Refactor board initialization and player switching
    Refactor TicTacToe class methods to use grid_size for board initialization and improve code structure.

--- src/TicTacToe.cpp
+++ src/TicTacToe.cpp
@@ -9,23 +9,13 @@ TicTacToe::TicTacToe(){
 
 // Stockage 
 void TicTacToe::Stockage(){
-<<<<<<< Updated upstream
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            Board[i][j] = '_';
-}
-    
-// Function to print the DashBoard
-void TicTacToe::DashBoard() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++)
-=======
     for (int i = 0; i < TicTacToe::grid_size; i++){
         for (int j = 0; j < TicTacToe::grid_size; j++){
             Board[i][j] = '_';
         }
     }
 }
+    
 
 // Function to print the Board
 
@@ -39,10 +29,7 @@ void TicTacToe::displayBoard(){
 }
 
 // Function to switch player
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 void TicTacToe::Switchplayer(){
     if(current_player == 'X')
         current_player = 'O';
@@ -51,15 +38,10 @@ void TicTacToe::Switchplayer(){
 }
 
 // Function to play
-<<<<<<< Updated upstream
-bool TicTacToe::Play(int line, int col){
-    if(Board[line][col] == '_'){
-=======
 
 bool TicTacToe::Play(int line, int col){
     // Assign the position if it's free else Nothing 
     if(Board[line][col] == '_'){          
->>>>>>> Stashed changes
         Board[line][col] = current_player;
         Switchplayer();
         return true;
@@ -69,11 +51,7 @@ bool TicTacToe::Play(int line, int col){
 }
 
 
-<<<<<<< Updated upstream
-// Function to show the win
-=======
 // Function to show the win // On it
->>>>>>> Stashed changes
 void TicTacToe::Win(int line, int col, char current_player){
 
     // Horizontal Win
