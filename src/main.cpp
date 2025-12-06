22e6f73bc10b388e07239d8d27e46dfb81b4ae77
maya <maya.sakata@imagino.com>
2025-12-06 20:20:37 +0100

    display tests in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,44 +5,52 @@
  */
 
 #include "TicTacToe.hpp"
+#include "Graphics.hpp"
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace std;
+using namespace sf;
 
 int main()
 {
-    int players, level, number_of_games;
-    cout << "\nWelcome to Tic Tac Toe!\n";
-    cout << "\n======================\n";
+    //     int players, level, number_of_games;
+    //     cout << "\nWelcome to Tic Tac Toe!\n";
+    //     cout << "\n======================\n";
 
-    cout << "How many players?\n1. Two Players (Human vs Human)\n2. One Player (vs Computer)\n\nEnter your choice (1-2): ";
-    cin >> players;
-    while (players < 1 || players > 2)
-    {
-        cout << "Only two options available 1 or 2" << endl;
-        cin >> players;
-    }
-    if (players == 2)
-    {
-        cout << "\nSelect difficulty level:\n1. Easy\n2. Hard (not implemented)\n\nEnter your choice (1-2): ";
-        cin >> level;
-        while (level < 1 || level > 2)
-        {
-            cout << "Only two levels available 1 or 2" << endl;
-            cin >> level;
-        }
-    }
-    else
-    {
-        level = 0; // Two players mode
-    }
-    cout << "\nHow many games in row? ";
-    cin >> number_of_games;
+    //     cout << "How many players?\n1. Two Players (Human vs Human)\n2. One Player (vs Computer)\n\nEnter your choice (1-2): ";
+    //     cin >> players;
+    //     while (players < 1 || players > 2)
+    //     {
+    //         cout << "Only two options available 1 or 2" << endl;
+    //         cin >> players;
+    //     }
+    //     if (players == 2)
+    //     {
+    //         cout << "\nSelect difficulty level:\n1. Easy\n2. Hard (not implemented)\n\nEnter your choice (1-2): ";
+    //         cin >> level;
+    //         while (level < 1 || level > 2)
+    //         {
+    //             cout << "Only two levels available 1 or 2" << endl;
+    //             cin >> level;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         level = 0; // Two players mode
+    //     }
+    //     cout << "\nHow many games in row? ";
+    //     cin >> number_of_games;
 
-    TicTacToe Game = TicTacToe(level, number_of_games);
+    //     TicTacToe Game = TicTacToe(level, number_of_games);
 
-    cout << "\nLet's play !\n " << endl;
+    //     cout << "\nLet's play !\n " << endl;
 
-    Game.Session(number_of_games);
+    //     Game.Session(number_of_games);
+
+    TicTacToe game = TicTacToe(1, 1);
+    Graphics graphics(game);
+    graphics.run();
 
     return 0;
 }
